ef1a1b861b906eda0b5396c26f3f5f033c8f550b
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-07-23 02:26:26 +0530

    notification bug fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import {sendCartData,fetchCartData} from './store/cart-actions';
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -17,18 +17,20 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   const dispatch = useDispatch();
-  
-   useEffect(()=>{
-    dispatch(fetchCartData())
-  },[dispatch]);
-
 
   useEffect(() => {
-    if(isInitial){
-      isInitial=false;
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
       return;
     }
-    dispatch(sendCartData(cart));
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
